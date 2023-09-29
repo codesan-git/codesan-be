51042c882244c3781d325130f6ea0ca7b2474da3
codesan-git <satrianugrahacode@gmail.com>
2023-09-29 08:52:31 +0700

    first commit

---
+++ src/user/user.service.ts
@@ -0,0 +1,51 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma.service';
+import { CreateUserDto } from './dto/dto/user.dto';
+import { hash } from 'bcrypt';
+
+@Injectable()
+export class UserService {
+  constructor(private prisma: PrismaService) {}
+
+  async create(dto: CreateUserDto) {
+    const userEmail = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    const userName = await this.prisma.user.findFirst({
+      where: {
+        name: dto.name,
+      },
+    });
+
+    if (userEmail) throw new ConflictException('email duplicated');
+    if (userName) throw new ConflictException('name duplicated');
+
+    const newUser = await this.prisma.user.create({
+      data: {
+        ...dto,
+        password: await hash(dto.password, 10),
+      },
+    });
+
+    const { password, ...result } = newUser;
+    return result;
+  }
+
+  async findByEmail(email: string) {
+    return await this.prisma.user.findUnique({
+      where: {
+        email: email,
+      },
+    });
+  }
+  async findById(id: number) {
+    return await this.prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+  }
+}
